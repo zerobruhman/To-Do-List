5d82e2f2f83cdb8e1c070f7972af997184ca50a8
zerobruhman <onebruhmanpc@gmail.com>
2026-06-09 20:37:25 +0700

    Update UI

--- app/views/todo/index.php
+++ app/views/todo/index.php
@@ -10,15 +10,19 @@ $todos = $todos ?? [];
 </head>
 <body>
     <h1>Selamat datang di TDL!</h1>
+    <h2>Tambahkan Todo</h2>
     <form method="POST" action="index.php?action=todo-store">
-        Judul: <input type="text" name="judul">
-        Deskripsi: <input type="text" name="deskripsi">
+        Judul: <input type="text" name="judul"> <br>
+        Deskripsi: <input type="text" name="deskripsi"> <br>
         <button type="submit" name="buat">Buat</button>
     </form>
+
+    <h2>Daftar Todo</h2>
     <?php foreach ($todos as $todo):?>
-    <h2>Judul: <?= htmlspecialchars($todo['judul']) ?></h2>
+    <h3>Judul: <?= htmlspecialchars($todo['judul']) ?></h2>
     <p>Deskripsi: <?= htmlspecialchars($todo['deskripsi']) ?></p>
     <p>Status: <?= $todo['status'] ?></p>
+    <button>Edit</button> <input type="checkbox"> <button>Hapus</button> 
     <?php endforeach?>
 </body>
 </html>
