150c748c57e9415f14aea7221835ee06135cc52a
zerobruhman <onebruhmanpc@gmail.com>
2026-06-10 21:11:05 +0700

    Memimprove views

--- app/views/todo/index.php
+++ app/views/todo/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . "/../../../core/CSRF.php";
 $todos = $todos ?? [];
 ?>
 <!DOCTYPE html>
@@ -12,17 +13,30 @@ $todos = $todos ?? [];
     <h1>Selamat datang di TDL!</h1>
     <h2>Tambahkan Todo</h2>
     <form method="POST" action="index.php?action=todo-store">
-        Judul: <input type="text" name="judul"> <br>
-        Deskripsi: <input type="text" name="deskripsi"> <br>
+        <input type="hidden" name="csrf_token" value="<?= CSRF::GenerateCsrftoken() ?>">
+        <div>
+            <label>Judul</label><br>
+            <input type="text" name="judul" required> <br>
+        </div>
+        <br>
+        <div>
+            <label>Deskripsi</label><br>
+            <textarea rows="4" name="deskripsi"></textarea> 
+        </div>
+        <br>
         <button type="submit" name="buat">Buat</button>
     </form>
 
     <h2>Daftar Todo</h2>
     <?php foreach ($todos as $todo):?>
-    <h3>Judul: <?= htmlspecialchars($todo['judul']) ?></h2>
-    <p>Deskripsi: <?= htmlspecialchars($todo['deskripsi']) ?></p>
-    <p>Status: <?= $todo['status'] ?></p>
-    <button>Edit</button> <input type="checkbox"> <button>Hapus</button> 
+        <div style="border: 10px solid #ccc; padding: 10px; margin-bottom: 10px;">
+            <h3><?= htmlspecialchars($todo['judul']) ?></h2>
+            <p>
+                <?= nl2br($todo['deskripsi']) ?>
+            </p>
+            <p>Status: <?= $todo['status'] ?></p>
+            <button>Edit</button> <input type="checkbox"> <button>Hapus</button> 
+        </div>
     <?php endforeach?>
 </body>
 </html>
